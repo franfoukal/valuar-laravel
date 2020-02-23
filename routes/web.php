67a3b384ad74d2731a7ea8fd5e9a5e06d3e177e5
damian8300 <dbouzo77@gmail.com>
2020-02-23 20:58:57 -0300

    fix login/register

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,12 @@
 use App\Http\Controllers\ProductController;
 
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/', function () {
-    return view('home');
-});
+
+
+Route::get('/', 'ProductController@bestSellers');
+
 Route::get('/home', 'ProductController@bestSellers');
+
 Route::get('/contact', function () {
     return view('contact');
 });
@@ -39,12 +40,8 @@ Route::get('/faq', function () {
     return view('faq');
 });
 Route::get('/product/{id}', 'ProductController@displayProduct');
-<<<<<<< HEAD
+
+
 Auth::routes();
 
-
-
-
-=======
 Route::get('/products', 'ProductController@productList');
->>>>>>> 0434553e8520c8e31b6859a972f428e81de53dfe
